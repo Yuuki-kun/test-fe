d8e84c766a54ed21c4503fe1fba7cf5cd57bcc06
Yuuki-kun <tongcongminhart@gmail.com>
2023-12-27 20:56:26 +0700

    new hash

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import { HashRouter, Routes, Route, Outlet } from "react-router-dom";
 import Apps from "./Apps";
 import Login from "./Login";
 
 function App() {
   return (
-    <BrowserRouter basename="/test-fe">
+    <HashRouter>
       <Routes>
         <Route path="/">
           <Route index element={<Apps />}></Route>
           <Route path="/login" element={<Login />}></Route>
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
--- src/Apps.jsx
+++ src/Apps.jsx
@@ -8,7 +8,7 @@ const Apps = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to.
+          Edit <code>src/App.js</code> and save tossss.
         </p>
         <a
           className="App-link"
